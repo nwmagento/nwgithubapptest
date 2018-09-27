9f477f066562f05c591edb08a153f59920e3604b
Nathan Wiering <nwiering@adobe.com>
2018-09-27 15:53:37 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+  // Your code here
+  app.log('Yay! The app was loaded!')
+
+  // example of probot responding 'Hello World' to a new issue being opened
+ app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+}
